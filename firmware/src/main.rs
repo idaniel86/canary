8fdfb61878170321de4202138720a00d201fe433
Daniel Igaz <daniel.igaz.86@gmail.com>
2026-08-12 14:16:34 +0200

    Set up workspace and initial firmware structure for the Canary project

---
+++ firmware/src/main.rs
@@ -0,0 +1,23 @@
+#![no_std]
+#![no_main]
+
+use {defmt_rtt as _, panic_probe as _}; // global logger + panicking-behavior
+
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_time::{Duration, Timer};
+use embassy_stm32::Config;
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    // Initialize the embassy runtime
+    let config = Config::default();
+    embassy_stm32::init(config);
+
+    info!("Hello World!");
+    
+    loop{
+        Timer::after(Duration::from_secs(1)).await;
+        None::<u32>.unwrap(); // This will cause a panic, which will be caught by the panic handler
+    }
+}
