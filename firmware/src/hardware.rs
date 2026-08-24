8744ccba8eac251d96f82ef7ab48bc4bd5712c4b
Daniel Igaz <daniel.igaz.86@gmail.com>
2026-08-24 09:23:21 +0200

    Configure RCC for 250 MHz system clock
    Initialize the system clock to 250 MHz using the following setup:
    - Utilize an 8 MHz external HSE in bypass mode (from ST-Link MCO).
    - Configure PLL1 to derive 250 MHz for the system clock.
    - Set the voltage scaling to Scale0 to support the maximum frequency.

--- firmware/src/hardware.rs
+++ firmware/src/hardware.rs
@@ -1,4 +1,4 @@
-use embassy_stm32::{Config, bind_interrupts, dma, i2c, mode, peripherals, time};
+use embassy_stm32::{Config, bind_interrupts, dma, i2c, mode, peripherals, rcc, time::Hertz};
 use embassy_sync::{blocking_mutex::raw::NoopRawMutex, mutex::Mutex};
 use static_cell::StaticCell;
 
@@ -20,7 +20,34 @@ pub struct Hardware<'d> {
 impl<'d> Default for Hardware<'d> {
     fn default() -> Self {
         // Initialize the embassy runtime
-        let config = Config::default();
+        let mut config = Config::default();
+        // Disable HSI, since we have an external clock source (ST-Link MCO)
+        config.rcc.hsi = None;
+        // Use HSE bypass mode, since we have an external clock source (ST-Link MCO)
+        config.rcc.hse = Some(rcc::Hse {
+            freq: Hertz(8_000_000),
+            mode: rcc::HseMode::BypassDigital,
+        });
+        // Configure PLL1 to generate 250 MHz system clock from HSE
+        config.rcc.pll1 = Some(rcc::Pll {
+            source: rcc::PllSource::HSE,   // Use HSE as PLL source
+            prediv: rcc::PllPreDiv::DIV2,  // Pre-divide HSE by 2 (8 MHz / 2 = 4 MHz)
+            mul: rcc::PllMul::MUL125,      // Multiply by 125 (4 MHz * 125 = 500 MHz)
+            divp: Some(rcc::PllDiv::DIV2), // Divide by 2 to get 250 MHz
+            divq: None,
+            divr: None,
+        });
+        // Set AHB prescaler to 1 (250 MHz)
+        config.rcc.ahb_pre = rcc::AHBPrescaler::DIV1;
+        // Set APB1, APB2, and APB3 prescalers to 1 (250 MHz)
+        config.rcc.apb1_pre = rcc::APBPrescaler::DIV1;
+        config.rcc.apb2_pre = rcc::APBPrescaler::DIV1;
+        config.rcc.apb3_pre = rcc::APBPrescaler::DIV1;
+        // Use PLL1_P as system clock
+        config.rcc.sys = rcc::Sysclk::PLL1_P;
+        // Set voltage scale to Scale0 for maximum frequency
+        config.rcc.voltage_scale = rcc::VoltageScale::Scale0;
+
         let p = embassy_stm32::init(config);
 
         // Initialize I2C2 in asynchronous master mode with DMA channels 4 and 5
